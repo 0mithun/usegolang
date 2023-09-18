922b01aa28765c30e7a043b383d45f07143da88d
Mithun Halder <mithun@infodigita.com>
2023-09-18 10:17:50 +0600

    Live reloading feature added

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello golang!<h1>")
+	fmt.Fprintf(w, "<h1>Welcome to my great site updat<h1>")
 }
 
 func main() {
