3c62220548862040325b3397a98ad8a4eec58863
Mithun Halder <mithunrptc@gmail.com>
2023-09-19 08:40:23 +0600

    Start using chi routes

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Welcome to my great site updat<h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", handlerFunc)
+
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+
 	fmt.Println("Starting server on port 3000")
-	http.ListenAndServe("localhost:3000", nil)
+	http.ListenAndServe("localhost:3000", r)
 }
